5727d436e4b6ee2c9129f3900e22933e105852fc
Dayem Saeed <dayems123@gmail.com>
2024-05-02 02:59:35 +0500

    improved UI and added chat and stuff to android

--- Tomo-Android/app/src/main/java/com/lumen/tomo/model/service/ChatService.kt
+++ Tomo-Android/app/src/main/java/com/lumen/tomo/model/service/ChatService.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 
 interface ChatService {
     @Headers(
-        "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6Y21laHJtbW5zdHFyb3Z3YWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjk3MzgsImV4cCI6MjAyNjgwNTczOH0.8UcMGluU4HlJqklHzMRn2aBjDQskr0mvY3c2b7w0PTs",
+        "Authorization: ${BuildConfig.SUPABASE_KEY}",
         "Content-Type: application/json"
     )
     @POST(BuildConfig.BOT_URI)
